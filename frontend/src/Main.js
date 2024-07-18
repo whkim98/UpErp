92b6d3b9241000e49da405ff5c71cb834c32c213
김우형 <hyeong7272@daum.net>
2024-07-18 12:04:40 +0900

    header

--- frontend/src/Main.js
+++ frontend/src/Main.js
@@ -4,22 +4,24 @@ import React from 'react';
 
 const Main = () => {
     return (
+        <div>
         <Box sx={{ display: 'flex', alignItems: 'flex-end' }} style={{}}>
         <SearchIcon sx={{ color: 'action.active', mr: 1, my: 0.5 }} />
-        <TextField id="input-with-sx" label="With sx" variant="standard" />
+        <TextField id="input-with-sx" label="메뉴 검색" variant="standard" />
         
-        <Button style={{color: 'black'}}>인사정보</Button>
-        <Button style={{color: 'black'}}>거래처등록</Button>
-        <Button style={{color: 'black'}}>품목등록</Button>
-        <Button style={{color: 'black'}}>구매입력</Button>
-        <Button style={{color: 'black'}}>판매입력</Button>
+        <Button style={{color: 'black'}}>인사관리</Button>
+        <Button style={{color: 'black'}}>거래처관리</Button>
+        <Button style={{color: 'black'}}>품목관리</Button>
+        <Button style={{color: 'black'}}>구매관리</Button>
+        <Button style={{color: 'black'}}>판매관리</Button>
             {/* <Backdrop
             sx={{ color: 'black', zIndex: (theme) => theme.zIndex.drawer + 1 }}
             >
             <CircularProgress color="inherit" />
             </Backdrop> */}
         </Box>
-
+        <hr/>
+        </div>
     );
 };
 
