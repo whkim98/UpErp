c5b894140adf7911e6994b75b165ab6c8256e595
김우형 <hyeong7272@daum.net>
2024-07-18 12:31:57 +0900

    test

--- backend/src/db/connection.js
+++ backend/src/db/connection.js
@@ -13,7 +13,7 @@ connection.connect(err => {
         console.error('Error connecting to the database:', err.stack);
         return;
     }
-    console.log('Connected to the database.');
+    console.log('DB연결성공');
 });
 
 export default connection;
