ceadc912bc16497d66e9bc3be8f578e632c94c82
김우형 <hyeong7272@daum.net>
2024-07-18 15:46:10 +0900

    login check

--- backend/src/routes/userRoutes.js
+++ backend/src/routes/userRoutes.js
@@ -5,15 +5,22 @@ import connection from '../db/connection.js';
 const router = express.Router();
 
 // 사용자 목록 조회 API
-router.get('/users', (req, res) => {
-    const query = 'SELECT * FROM test';
+router.get('/loginCheck', (req, res) => {
+    const { email, password } = req.query; // Assuming email and password are passed as query parameters
 
-    connection.query(query, (error, results) => {
+    const query = 'SELECT 1 FROM employees WHERE email = ? AND employee_pw = ?';
+
+    connection.query(query, [email, password], (error, results) => {
         if (error) {
             console.error('Error executing query:', error);
             return res.status(500).json({ error: 'Database query error' });
         }
-        res.json(results);
+
+        if (results.length > 0) {
+            res.json({ success: true });
+        } else {
+            res.status(401).json({ error: 'Invalid credentials' });
+        }
     });
 });
 
