5f3d75ce9266247fb7a5f54674d8bb1649e622d7
김우형 <hyeong7272@daum.net>
2024-07-18 14:15:15 +0900

    db connection change

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,7 +1,7 @@
-// src/server.js
 import express from 'express';
 import cors from 'cors';
 import userRoutes from './routes/userRoutes.js';
+import d3TestRoutes from './routes/d3Test.js';
 
 const app = express();
 const PORT = 3000;
@@ -11,6 +11,7 @@ app.use(express.json());
 
 // 라우터 사용
 app.use('/api', userRoutes);
+app.use('/api', d3TestRoutes);
 
 // 서버 시작
 app.listen(PORT, () => {
