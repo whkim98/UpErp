1f5302babf347a1f6076127ad0813bca1072b935
김우형 <hyeong7272@daum.net>
2024-07-24 10:14:52 +0900

    session table delete

--- backend/src/routes/userRoutes.js
+++ backend/src/routes/userRoutes.js
@@ -1,18 +1,16 @@
 import express from 'express';
 import connection from '../db/connection.js';
 import bodyParser from 'body-parser';
-import bcrypt from 'bcrypt'; // bcrypt 라이브러리 임포트
+import bcrypt from 'bcrypt';
 
 const router = express.Router();
 
-// Body parser 미들웨어
 router.use(bodyParser.json());
 router.use(bodyParser.urlencoded({ extended: true }));
 
 router.post('/loginCheck', async (req, res) => {
     const { email, employee_pw } = req.body;
 
-    // 이메일로 사용자 조회
     const query = 'SELECT employee_id, email, first_name, last_name, employee_pw FROM employees WHERE email = ?';
     connection.query(query, [email], async (error, results) => {
         if (error) {
@@ -23,15 +21,13 @@ router.post('/loginCheck', async (req, res) => {
         if (results.length > 0) {
             const user = results[0];
 
-            // 입력된 비밀번호와 저장된 해시 비밀번호 비교
             const match = await bcrypt.compare(employee_pw, user.employee_pw);
             if (match) {
-                // 세션에 사용자 정보 저장
                 req.session.user = {
                     id: user.employee_id,
                     email: user.email,
                     firstName: user.first_name,
-                    lastName: user.last_name
+                    lastName: user.last_name,
                 };
                 res.json({ success: true, user: req.session.user });
             } else {
@@ -43,8 +39,6 @@ router.post('/loginCheck', async (req, res) => {
     });
 });
 
-
-// 현재 사용자 정보 조회 API
 router.get('/currentUser', (req, res) => {
     if (req.session.user) {
         res.json({ success: true, user: req.session.user });
@@ -53,13 +47,21 @@ router.get('/currentUser', (req, res) => {
     }
 });
 
-// 로그아웃 API
 router.post('/logout', (req, res) => {
+    const session_id = req.sessionID; // 세션 ID 가져오기
     req.session.destroy((err) => {
         if (err) {
             return res.status(500).json({ error: '로그아웃 실패' });
         }
-        res.json({ success: true });
+
+        const sessionStore = req.app.locals.sessionStore; // app.locals에서 세션 스토어 가져오기
+        sessionStore.destroy(session_id, (err) => {
+            if (err) {
+                console.error('세션 삭제 에러:', err);
+                return res.status(500).json({ error: '세션 삭제 에러' });
+            }
+            res.json({ success: true });
+        });
     });
 });
 
