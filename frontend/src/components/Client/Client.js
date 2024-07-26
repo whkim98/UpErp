0d0fbba861ef12f00f9bff66e7d05d22e4d0d967
김우형 <hyeong7272@daum.net>
2024-07-26 16:33:42 +0900

    rename

--- frontend/src/components/Client/Client.js
+++ frontend/src/components/Client/Client.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import ReactVirtualizedTable from './ReactVirtualizedTable';
+import ReactVirtualizedTable from './clientListTable';
 
 const Client = () => {
     return (
